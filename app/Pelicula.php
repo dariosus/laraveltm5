dae40cd8edd3688f1345d699e4d52c5b6700c9be
dariosus <dario15th@gmail.com>
2017-11-07 11:19:25 -0300

    Modelos

---
+++ app/Pelicula.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pelicula extends Model
+{
+    protected $table = "movies";
+    //protected $primaryKey = "idPelicula";
+    //protected $timestamps = false;
+
+    //protected $fillable = ["id", "title"];
+    protected $guarded = [];
+}
